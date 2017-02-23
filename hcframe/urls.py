247fff817072d73e8ceae3abc40f430f35e99e17
Piyush Maurya <piyush.maurya@live.in>
2017-02-23 15:41:20 +0530

    file upload under testing

--- hcframe/urls.py
+++ hcframe/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from visual.views import file_input
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^upload/', file_input, name='upload'),
 ]
