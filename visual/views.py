5b3c1f524c4dd73a331ef465c8da32c50122756d
Piyush Maurya <piyush.maurya@live.in>
2017-02-23 15:03:14 +0530

    file upload added

--- visual/views.py
+++ visual/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import FileUploadForm
+from .models import FileUpload
+
+
+def file_input(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = FileUpload(file=request.FILES['file'])
+            file.save()
+
+    else:
+        form = FileUploadForm
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'visual/upload.html', context)
